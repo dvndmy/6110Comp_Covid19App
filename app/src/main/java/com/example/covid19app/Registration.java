058e6ffb16d346320d022cad9b2111aa7a79e975
JFBB6X <jfboyle97@gmail.com>
2020-11-15 23:48:16 +0000

    V 0.2

--- app/src/main/java/com/example/covid19app/Registration.java
+++ app/src/main/java/com/example/covid19app/Registration.java
@@ -24,11 +24,23 @@ public class Registration extends AppCompatActivity {
         Smoker              = (CheckBox)findViewById(R.id.cb_smoker);
         Medication          = (EditText)findViewById(R.id.et_medication);
         MedicalCondition    = (EditText)findViewById(R.id.et_medicalcondition);
-        Age                 = (EditText)findViewById(R.id.et_medicalcondition);
-        Weight              = (EditText)findViewById(R.id.et_medicalcondition);
+        Age                 = (EditText)findViewById(R.id.et_age);
+        Weight              = (EditText)findViewById(R.id.et_weight);
     }
 
    public void OnReg(View view) {
+       String  str_fullname              = FullName.getText().toString();
+       String  str_email                 = Email.getText().toString();
+       String  str_password              = Password.getText().toString();
+     /*Integer int_hospitalised          = Hospitalised
+       Integer int_smoker                = Smoker */
+       String   str_medication           = Medication.getText().toString();
+       String  str_medicalcondition      = MedicalCondition.getText().toString();
+       String  str_age                   = Age.getText().toString();
+       String  str_weight                = Weight.getText().toString();
+       String type = "register";
+       BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+       backgroundWorker.execute(type, str_fullname, str_email, str_password, int_hospitalised , int_smoker ,  str_medication,  str_medicalcondition,  str_age, str_weight       ;
        }
 
 }
