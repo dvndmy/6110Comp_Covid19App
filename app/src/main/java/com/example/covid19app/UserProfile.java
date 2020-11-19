767b7ea9116c5241ca5498606d55a454a2789fd9
Koskove <vumsoko@gmail.com>
2020-11-19 19:18:50 +0000

    Added Activites for User Profiles, Contact Doctor, Your Records, added styles.xml in values. Fixed constraints for activity_main.xml & activity_registration.xml

---
+++ app/src/main/java/com/example/covid19app/UserProfile.java
@@ -0,0 +1,39 @@
+package com.example.covid19app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+
+
+    public void gotoViewRecords(View view) {
+        Button ViewRecords = (Button) findViewById(R.id.btnViewRecords);
+
+        ViewRecords.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(UserProfile.this, YourRecords.class));
+            }
+        });
+
+    }
+
+    public void gotoUpdateRecords(View view) {
+    }
+
+    public void gotoContactDoctor(View view) {
+    }
+
+    public void gotoHelp(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/covid19app/YourRecords.java
@@ -0,0 +1,14 @@
+package com.example.covid19app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YourRecords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_records);
+    }
+}
