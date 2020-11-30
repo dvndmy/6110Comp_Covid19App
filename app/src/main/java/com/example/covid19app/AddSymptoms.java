39205aa6171adfe46debd8ee7bb16f0c75f28658
Koskove <vumsoko@gmail.com>
2020-11-30 22:02:05 +0000

    Added backuserPage method in AddSymptoms.java

--- app/src/main/java/com/example/covid19app/AddSymptoms.java
+++ app/src/main/java/com/example/covid19app/AddSymptoms.java
@@ -2,13 +2,30 @@ package com.example.covid19app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddSymptoms extends AppCompatActivity {
+    private Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_symptoms);
+
+        backBtn = (Button) findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backuserPage();
+            }
+        });
+    }
+
+    public void backuserPage() {
+        Intent intent = new Intent(this, UserPage.class);
+        startActivity(intent);
     }
 }
